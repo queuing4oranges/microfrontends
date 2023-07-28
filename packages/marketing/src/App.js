657a59593fe565d5a438d27c7ce0a771499dc626
queuing4oranges <katjazenker@gmail.com>
2023-07-28 17:52:58 +0200

    prepend classnames

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+//this will prefix all the generated classnames (to prevent doubles)
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      {/* stylesprovider is a react component, that is used to customize css */}
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
